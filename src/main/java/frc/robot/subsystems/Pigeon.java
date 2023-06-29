5973e0458eb263dde6ffe03865186c13085138d0
Armaan Kakkar <Armaan.kakkar07@gmail.com>
2023-06-29 01:05:34 -0500

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Pigeon.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.Pigeon2;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.Telemetry;
+
+public class Pigeon extends SubsystemBase {
+  private Pigeon2 pigeon;
+
+  private double yaw = 0;
+  private double pitch = 0;
+  private double roll = 0;
+  private double[] ypr = new double[3];
+
+    /** Creates a new ExampleSubsystem. */
+  public Pigeon () {
+    pigeon = new Pigeon2(1, "drivetrain");
+  }
+
+  public Pigeon2 getPigeon () {
+      return pigeon;
+  }
+
+  public double getYaw() {
+    return yaw % 360;
+  }
+
+  public double getPitch () {
+    return pitch;
+  }
+
+  public double getRoll () {
+    return roll;
+  }
+
+  public double[] getYPR () {
+    return ypr;
+  }
+
+  public void zeroYaw () {
+    pigeon.setYaw(0);
+  }
+
+  public void zeroYaw (double degrees) {
+    pigeon.setYaw(degrees);
+  }
+
+  @Override
+  public void periodic() {
+    yaw = pigeon.getYaw() % 360;
+    pitch = pigeon.getPitch();
+    roll = pigeon.getRoll();
+    pigeon.getYawPitchRoll(ypr);
+
+    Telemetry.setValue("drivetrain/gyro/temperature", pigeon.getTemp());
+    Telemetry.setValue("drivetrain/gyro/yaw", yaw);
+    Telemetry.setValue("drivetrain/gyro/roll", roll);
+    Telemetry.setValue("drivetrain/gyro/pitch", pitch);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
